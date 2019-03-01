4cc51d7360391adb76a4c6993621c12a4a2a226d
Greg Schivley <greg.schivley@gmail.com>
2019-02-28 23:04:29 -0500

    Fix param name and add ba2ba function

--- electricitylci/bulk_eia_data.py
+++ electricitylci/bulk_eia_data.py
@@ -40,7 +40,7 @@ EXCHANGE_ROWS = [
 
 BA_TO_BA_ROWS = [
     row for row in EXCHANGE_ROWS
-    if row['series_id'].split('-')[0][4:] not in region_acronyms
+    if row['series_id'].split('-')[0][4:] not in REGION_ACRONYMS
 ]
 
 
@@ -76,6 +76,52 @@ def row_to_df(rows, data_type):
 
         df_data = {
             'region': region,
+            'datetime': datetime,
+            data_type: data,
+        }
+
+        _df = pd.DataFrame(df_data)
+        df_list.append(_df)
+
+    df = pd.concat(df_list).reset_index(drop=True)
+
+    return df
+
+
+def ba_exchange_to_df(rows, data_type='ba_to_ba'):
+    """
+    Turn rows of a single type from the bulk data text file into a dataframe
+    with the region, datetime, and data as columns
+
+    Parameters
+    ----------
+    rows : list
+        rows from the EBA.txt file
+    data_type : str
+        name to use for the data column (e.g. demand or total_interchange)
+
+    Returns
+    -------
+    dataframe
+        Data for all regions in a single df with datatimes converted and UTC
+    """
+    df_list = []
+    for row in rows:
+
+        # "data" is of form:
+        # [['20190214T04Z', -102],
+        # ['20190214T03Z', -107],
+        # ['20190214T02Z', -108],
+        # ['20190214T01Z', -103]]
+        datetime = pd.to_datetime([x[0] for x in row['data']],
+                                  utc=True, format='%Y%m%dT%HZ')
+        data = [x[1] for x in row['data']]
+        from_region = row['series_id'].split('-')[0][4:]
+        to_region = row['series_id'].split('-')[1][:-5]
+
+        df_data = {
+            'from_region': from_region,
+            'to_region': to_region,
             'datetime': datetime,
             data_type: data,
         }
